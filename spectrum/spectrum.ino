b6c133adc4db0b1b33a7006b1bb19144b3ea699a
Tony DiCola <tony@tonydicola.com>
2013-10-01 20:21:30 -0700

    Update spectrum display sketch.

--- spectrum/spectrum.ino
+++ spectrum/spectrum.ino
@@ -1,4 +1,4 @@
-// Spectrum Display
+// Audio Spectrum Display
 // Copyright 2013 Tony DiCola (tony@tonydicola.com)
 
 // This code is meant to run on a Teensy 3.0 microcontroller which is hooked up to
@@ -16,7 +16,11 @@
 // These values can be changed to alter the behavior of the spectrum display.
 ////////////////////////////////////////////////////////////////////////////////
 
-int SAMPLE_RATE_HZ = 9000;
+int SAMPLE_RATE_HZ = 9000;             // Sample rate of the audio in hertz.
+float SPECTRUM_MIN_DB = 30.0;          // Audio intensity (in decibels) that maps to low LED brightness.
+float SPECTRUM_MAX_DB = 60.0;          // Audio intensity (in decibels) that maps to high LED brightness.
+int LEDS_ENABLED = 1;                  // Control if the LED's should display the spectrum or not.  1 is true, 0 is false.
+                                       // Useful for turning the LED display on and off with commands from the serial port.
 const int FFT_SIZE = 256;              // Size of the FFT.  Realistically can only be at most 256 
                                        // without running out of memory for buffers and other state.
 const int AUDIO_INPUT_PIN = 14;        // Input ADC pin for audio data.
@@ -24,7 +28,8 @@ const int ANALOG_READ_RESOLUTION = 10; // Bits of resolution for the ADC.
 const int ANALOG_READ_AVERAGING = 16;  // Number of samples to average with each ADC reading.
 const int POWER_LED_PIN = 13;          // Output pin for power LED (pin 13 to use Teensy 3.0's onboard LED).
 const int NEO_PIXEL_PIN = 3;           // Output pin for neo pixels.
-const int NEO_PIXEL_COUNT = 4;         // Number of neo pixels.
+const int NEO_PIXEL_COUNT = 4;         // Number of neo pixels.  You should be able to increase this without
+                                       // any other changes to the program.
 const int MAX_CHARS = 65;              // Max size of the input command buffer
 
 
@@ -39,6 +44,8 @@ float magnitudes[FFT_SIZE];
 int sampleCounter = 0;
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NEO_PIXEL_COUNT, NEO_PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 char commandBuffer[MAX_CHARS];
+float frequencyWindow[NEO_PIXEL_COUNT+1];
+float hues[NEO_PIXEL_COUNT];
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -65,6 +72,9 @@ void setup() {
   // Clear the input command buffer
   memset(commandBuffer, 0, sizeof(commandBuffer));
   
+  // Initialize spectrum display
+  spectrumSetup();
+  
   // Begin sampling audio
   samplingBegin();
 }
@@ -78,6 +88,11 @@ void loop() {
     arm_cfft_radix4_f32(&fft_inst, samples);
     // Calculate magnitude of complex numbers output by the FFT.
     arm_cmplx_mag_f32(samples, magnitudes, FFT_SIZE);
+  
+    if (LEDS_ENABLED == 1)
+    {
+      spectrumLoop();
+    }
   
     // Restart audio sampling.
     samplingBegin();
@@ -114,6 +129,90 @@ int frequencyToBin(float frequency) {
   return int(frequency / binFrequency);
 }
 
+// Convert from HSV values (in floating point 0 to 1.0) to RGB colors usable
+// by neo pixel functions.
+uint32_t pixelHSVtoRGBColor(float hue, float saturation, float value) {
+  // Implemented from algorithm at http://en.wikipedia.org/wiki/HSL_and_HSV#From_HSV
+  float chroma = value * saturation;
+  float h1 = float(hue)/60.0;
+  float x = chroma*(1.0-fabs(fmod(h1, 2.0)-1.0));
+  float r = 0;
+  float g = 0;
+  float b = 0;
+  if (h1 < 1.0) {
+    r = chroma;
+    g = x;
+  }
+  else if (h1 < 2.0) {
+    r = x;
+    g = chroma;
+  }
+  else if (h1 < 3.0) {
+    g = chroma;
+    b = x;
+  }
+  else if (h1 < 4.0) {
+    g = x;
+    b = chroma;
+  }
+  else if (h1 < 5.0) {
+    r = x;
+    b = chroma;
+  }
+  else // h1 <= 6.0
+  {
+    r = chroma;
+    b = x;
+  }
+  float m = value - chroma;
+  r += m;
+  g += m;
+  b += m;
+  return pixels.Color(int(255*r), int(255*g), int(255*b));
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// SPECTRUM DISPLAY FUNCTIONS
+///////////////////////////////////////////////////////////////////////////////
+
+void spectrumSetup() {
+  // Set the frequency window values by evenly dividing the possible frequency
+  // spectrum across the number of neo pixels.
+  float windowSize = (SAMPLE_RATE_HZ / 2.0) / float(NEO_PIXEL_COUNT);
+  for (int i = 0; i < NEO_PIXEL_COUNT+1; ++i) {
+    frequencyWindow[i] = i*windowSize;
+  }
+  // Evenly spread hues across all pixels.
+  for (int i = 0; i < NEO_PIXEL_COUNT; ++i) {
+    hues[i] = 360.0*(float(i)/float(NEO_PIXEL_COUNT-1));
+  }
+}
+
+void spectrumLoop() {
+  // Update each LED based on the intensity of the audio 
+  // in the associated frequency window.
+  float intensity, otherMean;
+  for (int i = 0; i < NEO_PIXEL_COUNT; ++i) {
+    // Notice the first magnitude value is skipped because it represents average
+    // power of the signal.
+    windowMean(magnitudes+1, 
+               frequencyToBin(frequencyWindow[i]),
+               frequencyToBin(frequencyWindow[i+1]),
+               &intensity,
+               &otherMean);
+    // Convert intensity to decibels.
+    intensity = 20.0*log10(intensity);
+    // Scale the intensity and clamp between 0 and 1.0.
+    intensity -= SPECTRUM_MIN_DB;
+    intensity = intensity < 0.0 ? 0.0 : intensity;
+    intensity /= (SPECTRUM_MAX_DB-SPECTRUM_MIN_DB);
+    intensity = intensity > 1.0 ? 1.0 : intensity;
+    pixels.setPixelColor(i, pixelHSVtoRGBColor(hues[i], 1.0, intensity));
+  }
+  pixels.show();
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // SAMPLING FUNCTIONS
@@ -146,8 +245,16 @@ boolean samplingIsDone() {
 ////////////////////////////////////////////////////////////////////////////////
 // COMMAND PARSING FUNCTIONS
 // These functions allow parsing simple commands input on the serial port.
+// Commands allow reading and writing variables that control the device.
 //
 // All commands must end with a semicolon character.
+// 
+// Example commands are:
+// GET SAMPLE_RATE_HZ;
+// - Get the sample rate of the device.
+// SET SAMPLE_RATE_HZ 400;
+// - Set the sample rate of the device to 400 hertz.
+// 
 ////////////////////////////////////////////////////////////////////////////////
 
 void parserLoop() {
@@ -169,6 +276,15 @@ void parserLoop() {
   }
 }
 
+// Macro used in parseCommand function to simplify parsing get and set commands for a variable
+#define GET_AND_SET(variableName) \
+  else if (strcmp(command, "GET " #variableName) == 0) { \
+    Serial.println(variableName); \
+  } \
+  else if (strstr(command, "SET " #variableName " ") != NULL) { \
+    variableName = (typeof(variableName)) atof(command+(sizeof("SET " #variableName " ")-1)); \
+  }
+
 void parseCommand(char* command) {
   if (strcmp(command, "GET MAGNITUDES") == 0) {
     for (int i = 0; i < FFT_SIZE; ++i) {
@@ -183,10 +299,21 @@ void parseCommand(char* command) {
   else if (strcmp(command, "GET FFT_SIZE") == 0) {
     Serial.println(FFT_SIZE);
   }
-  else if (strcmp(command, "GET SAMPLE_RATE_HZ") == 0) { 
-    Serial.println(SAMPLE_RATE_HZ); 
-  } 
-  else if (strstr(command, "SET SAMPLE_RATE_HZ ") != NULL) { 
-    SAMPLE_RATE_HZ = atoi(command+(sizeof("SET SAMPLE_RATE_HZ ")-1)); 
-  }  
+  GET_AND_SET(SAMPLE_RATE_HZ)
+  GET_AND_SET(LEDS_ENABLED)
+  GET_AND_SET(SPECTRUM_MIN_DB)
+  GET_AND_SET(SPECTRUM_MAX_DB)
+  
+  // Update spectrum display values if sample rate was changed.
+  if (strstr(command, "SET SAMPLE_RATE_HZ ") != NULL) {
+    spectrumSetup();
+  }
+  
+  // Turn off the LEDs if the state changed.
+  if (LEDS_ENABLED == 0) {
+    for (int i = 0; i < NEO_PIXEL_COUNT; ++i) {
+      pixels.setPixelColor(i, 0);
+    }
+    pixels.show();
+  }
 }

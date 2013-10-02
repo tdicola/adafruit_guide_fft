6cfb1f7a386faad0096fc75444d99297a5fc083c
Tony DiCola <tony@tonydicola.com>
2013-10-01 18:55:23 -0700

    Initial spectrum analyzer sketch checkin.

---
+++ spectrum/spectrum.ino
@@ -0,0 +1,192 @@
+// Spectrum Display
+// Copyright 2013 Tony DiCola (tony@tonydicola.com)
+
+// This code is meant to run on a Teensy 3.0 microcontroller which is hooked up to
+// a microphone (such as http://www.adafruit.com/products/1063) and 4 neo pixels
+// (from http://www.adafruit.com/products/1260).  The pins for these inputs & outputs
+// can be adjusted in the configuration variables below.
+
+#define ARM_MATH_CM4
+#include <arm_math.h>
+#include <Adafruit_NeoPixel.h>
+
+
+////////////////////////////////////////////////////////////////////////////////
+// CONIFIGURATION 
+// These values can be changed to alter the behavior of the spectrum display.
+////////////////////////////////////////////////////////////////////////////////
+
+int SAMPLE_RATE_HZ = 9000;
+const int FFT_SIZE = 256;              // Size of the FFT.  Realistically can only be at most 256 
+                                       // without running out of memory for buffers and other state.
+const int AUDIO_INPUT_PIN = 14;        // Input ADC pin for audio data.
+const int ANALOG_READ_RESOLUTION = 10; // Bits of resolution for the ADC.
+const int ANALOG_READ_AVERAGING = 16;  // Number of samples to average with each ADC reading.
+const int POWER_LED_PIN = 13;          // Output pin for power LED (pin 13 to use Teensy 3.0's onboard LED).
+const int NEO_PIXEL_PIN = 3;           // Output pin for neo pixels.
+const int NEO_PIXEL_COUNT = 4;         // Number of neo pixels.
+const int MAX_CHARS = 65;              // Max size of the input command buffer
+
+
+////////////////////////////////////////////////////////////////////////////////
+// INTERNAL STATE
+// These shouldn't be modified unless you know what you're doing.
+////////////////////////////////////////////////////////////////////////////////
+
+IntervalTimer samplingTimer;
+float samples[FFT_SIZE*2];
+float magnitudes[FFT_SIZE];
+int sampleCounter = 0;
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NEO_PIXEL_COUNT, NEO_PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+char commandBuffer[MAX_CHARS];
+
+
+////////////////////////////////////////////////////////////////////////////////
+// MAIN SKETCH FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+void setup() {
+  // Set up serial port.
+  Serial.begin(38400);
+  
+  // Set up ADC and audio input.
+  pinMode(AUDIO_INPUT_PIN, INPUT);
+  analogReadResolution(ANALOG_READ_RESOLUTION);
+  analogReadAveraging(ANALOG_READ_AVERAGING);
+  
+  // Turn on the power indicator LED.
+  pinMode(POWER_LED_PIN, OUTPUT);
+  digitalWrite(POWER_LED_PIN, HIGH);
+  
+  // Initialize neo pixel library and turn off the LEDs
+  pixels.begin();
+  pixels.show(); 
+  
+  // Clear the input command buffer
+  memset(commandBuffer, 0, sizeof(commandBuffer));
+  
+  // Begin sampling audio
+  samplingBegin();
+}
+
+void loop() {
+  // Calculate FFT if a full sample is available.
+  if (samplingIsDone()) {
+    // Run FFT on sample data.
+    arm_cfft_radix4_instance_f32 fft_inst;
+    arm_cfft_radix4_init_f32(&fft_inst, FFT_SIZE, 0, 1);
+    arm_cfft_radix4_f32(&fft_inst, samples);
+    // Calculate magnitude of complex numbers output by the FFT.
+    arm_cmplx_mag_f32(samples, magnitudes, FFT_SIZE);
+  
+    // Restart audio sampling.
+    samplingBegin();
+  }
+    
+  // Parse any pending commands.
+  parserLoop();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// UTILITY FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+// Compute the average magnitude of a target frequency window vs. all other frequencies.
+void windowMean(float* magnitudes, int lowBin, int highBin, float* windowMean, float* otherMean) {
+    *windowMean = 0;
+    *otherMean = 0;
+    for (int i = 1; i < FFT_SIZE/2; ++i) {
+      if (i >= lowBin && i <= highBin) {
+        *windowMean += magnitudes[i];
+      }
+      else {
+        *otherMean += magnitudes[i];
+      }
+    }
+    *windowMean /= (highBin - lowBin) + 1;
+    *otherMean /= (FFT_SIZE / 2 - (highBin - lowBin));
+}
+
+// Convert a frequency to the appropriate FFT bin it will fall within.
+int frequencyToBin(float frequency) {
+  float binFrequency = float(SAMPLE_RATE_HZ) / float(FFT_SIZE);
+  return int(frequency / binFrequency);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// SAMPLING FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+void samplingCallback() {
+  // Read from the ADC and store the sample data
+  samples[sampleCounter] = (float32_t)analogRead(AUDIO_INPUT_PIN);
+  // Complex FFT functions require a coefficient for the imaginary part of the input.
+  // Since we only have real data, set this coefficient to zero.
+  samples[sampleCounter+1] = 0.0;
+  // Update sample buffer position and stop after the buffer is filled
+  sampleCounter += 2;
+  if (sampleCounter >= FFT_SIZE*2) {
+    samplingTimer.end();
+  }
+}
+
+void samplingBegin() {
+  // Reset sample buffer position and start callback at necessary rate.
+  sampleCounter = 0;
+  samplingTimer.begin(samplingCallback, 1000000/SAMPLE_RATE_HZ);
+}
+
+boolean samplingIsDone() {
+  return sampleCounter >= FFT_SIZE*2;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// COMMAND PARSING FUNCTIONS
+// These functions allow parsing simple commands input on the serial port.
+//
+// All commands must end with a semicolon character.
+////////////////////////////////////////////////////////////////////////////////
+
+void parserLoop() {
+  // Process any incoming characters from the serial port
+  while (Serial.available() > 0) {
+    char c = Serial.read();
+    // Add any characters that aren't the end of a command (semicolon) to the input buffer.
+    if (c != ';') {
+      c = toupper(c);
+      strncat(commandBuffer, &c, 1);
+    }
+    else
+    {
+      // Parse the command because an end of command token was encountered.
+      parseCommand(commandBuffer);
+      // Clear the input buffer
+      memset(commandBuffer, 0, sizeof(commandBuffer));
+    }
+  }
+}
+
+void parseCommand(char* command) {
+  if (strcmp(command, "GET MAGNITUDES") == 0) {
+    for (int i = 0; i < FFT_SIZE; ++i) {
+      Serial.println(magnitudes[i]);
+    }
+  }
+  else if (strcmp(command, "GET SAMPLES") == 0) {
+    for (int i = 0; i < FFT_SIZE*2; i+=2) {
+      Serial.println(samples[i]);
+    }
+  }
+  else if (strcmp(command, "GET FFT_SIZE") == 0) {
+    Serial.println(FFT_SIZE);
+  }
+  else if (strcmp(command, "GET SAMPLE_RATE_HZ") == 0) { 
+    Serial.println(SAMPLE_RATE_HZ); 
+  } 
+  else if (strstr(command, "SET SAMPLE_RATE_HZ ") != NULL) { 
+    SAMPLE_RATE_HZ = atoi(command+(sizeof("SET SAMPLE_RATE_HZ ")-1)); 
+  }  
+}
